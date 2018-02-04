4b19637ab5fdaf3b9a073069fddc9bcf85e1d0b2
chengang <372763861@qq.com>
2018-02-04 21:59:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-/bin/frps -c /frps.ini
+inputinfo=$1
+if [ "$inputinfo" = "" ]; then
+cd /root/bcdn/BerryMiner_v1_0/server
+./bcdn_check.sh
+else
+echo -ne "$1" > /root/bcdn/BerryMiner_v1_0/server/conf/code.txt
+cd /root/bcdn/BerryMiner_v1_0/server
+./bcdn_check.sh
+fi
